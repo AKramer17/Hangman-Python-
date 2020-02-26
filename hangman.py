49016cd275e9626c5eaafe93c49c539a2d7a31a5
Alex Kramer <kramer612@gmail.com>
2020-02-26 13:25:28 -0500

    Setting up user input

--- hangman.py
+++ hangman.py
@@ -1,10 +1,20 @@
 import random
 
 def hangman():
-    print "Welcome to Hangman!"
-    wordDash = "_" *len(theWord)
+    print ("Welcome to Hangman!")
+    wordDash = "_" *len(hiddenWord)
+    numGuesses = 0
+    remainingGuesses = 9 - numGuesses
 
+    while remainingGuesses != 0:
+        print (wordDash)
+        print ("Remaining guesses: " + remainingGuesses)
 
-wordList = ["alligator", "astroid", "balloon"]
-theWord = random.choice(wordList)
+        letter = input("Guess: ")
+        if len(letter) != 1 or letter != #letter.toString() -- how to do in Python
+
+    
+
+wordList = ["alligator", "astroid", "balloon"] #To be extended
+hiddenWord = random.choice(wordList)
 hangman()
