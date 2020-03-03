444b5f7f9a1a05e301e0378542fc609ba216e197
Alex Kramer <kramer612@gmail.com>
2020-03-02 20:23:29 -0500

    Fill in blanks

--- hangman.py
+++ hangman.py
@@ -11,9 +11,15 @@ def hangman():
         print ("Remaining guesses: " + remainingGuesses)
 
         letter = input("Guess: ")
-        if len(letter) != 1 or letter != str(letter):
+        if len(letter) != 1 or letter != str(letter): #Make sure user enters a letter
             print ("Invalid entry. Please enter a letter.")
+        
+        elif letter in hiddenWord:
+            wordDash = letterFill
 
+def letterFill(hiddenWord, wordDash, letter):
+    for i in range(len(hiddenWord)):
+        if hiddenWord[i] == letter:
 
 wordList = ["alligator", "astroid", "balloon"] #To be extended
 hiddenWord = random.choice(wordList)
